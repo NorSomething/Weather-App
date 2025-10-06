3185cf617b7e7d3db01f6d80c9f36e6022adeb1d
nirmal <mcnirmalkumar00@gmail.com>
2025-10-06 22:50:57 +0530

    added gui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import tkinter as tk
+import customtkinter as ctk
 import os
 from tkinter import messagebox
 from tkinter import PhotoImage
@@ -7,33 +8,58 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+class Weather_GUI:
 
-API_KEY = os.getenv('API_KEY')
-print("Loaded API Key :", API_KEY)
+    def __init__(self):
 
-city = input("Enter City : ")
+        self.API_KEY = os.getenv('API_KEY')
+        
+        self.root = ctk.CTk()
 
-def get_weather_data(ci):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={ci}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
+        self.root.geometry('800x600')
+        self.root.title('WeatherApp')
 
-    if response.status_code == 200:
-        weather_data = response.json()
-        return weather_data
-    else:
-        pass
+        #city = input("Enter City : ")
+
+        self.label = ctk.CTkLabel(self.root, text='Weather App', font=('Arial', 55))
+        self.label.pack(padx=10, pady=10)
+
+        self.textbox = ctk.CTkTextbox(self.root, height=10, font=('Arial', 45))
+        self.textbox.pack(padx=10, pady=10)
+
+        self.check_state = ctk.IntVar()
+
+        self.button = ctk.CTkButton(self.root, text='Get Weather Information', command=self.get_info, font=('Arial', 30))
+        self.button.pack(padx=20, pady=10)
+
+        self.label_current_temp = ctk.CTkLabel(self.root, text="")
+        self.label_current_temp.pack(padx=10, pady=10, anchor='w')
+
+        self.root.mainloop()
+
+    def get_weather_data(self, ci):
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={ci}&appid={self.API_KEY}&units=metric"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            weather_data = response.json()
+            return weather_data
+        else:
+            pass
+        
+    def get_info(self):
+        self.city_name = self.textbox.get('1.0', 'end').strip().lower()
+        self.weather_info = self.get_weather_data(self.city_name)
+            
+        current_temp = self.weather_info['main']["temp"]
+
+        self.label_current_temp.configure(text=f"Current Temperature in {self.city_name.title()} is : {current_temp} C.", font=('Arial', 30))
 
 
 
-weather_info = get_weather_data(city)
+        #weather_info = get_weather_data(city)
 
-if weather_info:
-    print(f"Current Temperature is {weather_info['main']['temp']} Celcius.")
-    print(f"Pressure is {weather_info['main']['pressure']} bar.")
-    print(f"Humidity is {weather_info['main']['humidity']} percent.")
-    print(f"Wind speed is {weather_info['wind']['speed']} km/h.")
-else:
-    print(f"Unable to fetch weather info for {city}.")
+Weather_GUI()
 
 
     
